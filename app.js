09c1d6409233ea29b6ce1093e682687993565cc8
solo5star <solo_5@naver.com>
2020-06-28 05:50:55 +0000

    Support subdomain (Could use reverse proxy) and added config.ini

--- app.js
+++ app.js
@@ -13,6 +13,13 @@ const Gamedig = require('gamedig');
 const QueryService = require(__basedir + '/lib/query-service');
 const express = require('express');
 const bodyParser = require('body-parser');
+const iniparser = require('iniparser');
+
+/**
+ * Parse config.ini
+ */
+const config = iniparser.parseSync(__basedir + '/config/config.ini');
+console.log("config.ini = " + JSON.stringify(config, null, 2));
 
 /**
  * Initialize express app
@@ -26,18 +33,29 @@ app.set('json spaces', 2);
 /**
  * Set up routes
  */
+const root = express.Router();
 const api = require(__basedir + '/routes/api');
 const home = require(__basedir + '/routes/home');
 
-app.use('/', express.static(__basedir + '/public'));
-app.use('/', home);
-app.use('/api', api);
+if((config.subdomain.enable || "false").toLowerCase() == "true"){
+	console.log(`[McbeRank] Subdomain is enabled = ${config.subdomain.subdomain}`)
+	app.get('/', (req, res) => res.redirect(`/${config.subdomain.subdomain}`));
+	app.use(`/${config.subdomain.subdomain}`, root);
+}else{
+	app.use('/', root);
+}
+root.use('/', express.static(__basedir + '/public'));
+root.use('/', home);
+root.use('/api', api);
 
 /**
  * Start services
  */
 QueryService.start();
 
-app.listen(3500, function(){
-	console.log("[McbeRank] Server is now running on port 3500");
-});
+app.listen(
+	config.server.port || 3500,
+	config.server.bind || "0.0.0.0",
+	function(){
+		console.log(`[McbeRank] Server is now running on port ${config.server.bind}:${config.server.port}`);
+	});
