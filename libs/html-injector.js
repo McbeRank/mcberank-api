b02bc88b7a7247231e0026695e37c06a32a1fc01
solo5star <solo_5@naver.com>
2020-08-17 06:39:50 +0000

    Promisify html injector

--- libs/html-injector.js
+++ libs/html-injector.js
@@ -1,21 +1,15 @@
 const { JSDOM } = require('jsdom');
 const fs = require('fs');
+const util = require('util');
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
 
 const Injector = {
     async injectBaseHref(file, baseHref){
-        new Promise(resolve => fs.readFile(file, 'utf8', (err, data) => {
-            if(err) throw err;
-
-            var dom = new JSDOM(data);
-            dom.window.document.querySelector('head > base').setAttribute('href', baseHref);
-
-            fs.writeFile(file, dom.serialize(), err => {
-                if(err) throw err;
-
-                logger.info('HTML Injector: successfully inject base href');
-                resolve();
-            });
-        }));
+        var dom = new JSDOM(await readFile(file, 'utf8'));
+        dom.window.document.querySelector('head > base').setAttribute('href', baseHref);
+        writeFile(file, dom.serialize());
+        logger.info('HTML Injector: successfully inject base href');
     }
 }
 
