c635ca6442bd316ea24178066f909637e866b6a5
dokidia <42838268+twk1024@users.noreply.github.com>
2020-12-07 00:19:48 +0900

    motd check option

--- controllers/controller.servers.js
+++ controllers/controller.servers.js
@@ -3,6 +3,8 @@ const Server = mongoose.model('Server');
 const { NotFound, UnprocessableEntity } = require('http-errors');
 const controller = {};
 
+global.config = require('./libs/config');
+
 controller.paramServer = async function(req, res, next, server){
     req.server = await Server.findOne({ slug: server })
         .populate('plugins')
@@ -34,8 +36,10 @@ controller.createServer = async function(req, res){
     await server.query1();
     if(!server.online) throw new UnprocessableEntity('서버가 오프라인입니다. 서버 상태를 확인해주세요.');
 
+    if(config.get('registration.motd_check') == 'true'){
     // Check whether the server belongs to the registrant or not
-    if(server.title !== 'MCBE_SERVER') throw new UnprocessableEntity('서버의 MOTD가 MCBE_SERVER가 아닙니다.');
+         if(server.title !== 'MCBE_SERVER') throw new UnprocessableEntity('서버의 MOTD가 MCBE_SERVER가 아닙니다.');
+    }
     
     await server.save();
 
