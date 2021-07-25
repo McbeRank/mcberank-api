fd65667b79fae93e11fcd581fa9758ad80614645
solo5star <solo_5@naver.com>
2021-07-26 04:05:53 +0900

    influxdb.js: wait for port

--- libs/database/influxdb.js
+++ libs/database/influxdb.js
@@ -1,37 +1,43 @@
 const Influx = require('influx');
+const waitForPort = require('wait-for-port');
 
 const [ host, port ] = config.get('influxdb.host').split(':');
 const username = config.get('influxdb.username');
 const password = config.get('influxdb.password');
 const database = config.get('influxdb.database');
 
-logger.info(`InfluxDB: connect to ${username}:******@${host}:${port}/${database}`);
+logger.info(`InfluxDB: connect to ${username}@${host}:${port}/${database}`);
 
-const influx = new Influx.InfluxDB({
-    host,
-    port,
-    username,
-    password,
-    database,
-    schema: [
-        {
-            measurement: 'servers',
-            fields: {
-                online: Influx.FieldType.BOOLEAN,
-                numplayers: Influx.FieldType.INTEGER
-            },
-            tags: [ 'server' ]
-        }
-    ]
-});
+const influx = {};
 
-(async () => {
-    if(!(await influx.getDatabaseNames()).includes(database)){
+// influx.waitForConnection = influx.ping(5000);
+influx.waitForConnection = async () => {
+    await waitForPort(port);
+
+    Object.assign(influx, new Influx.InfluxDB({
+        host,
+        port,
+        username,
+        password,
+        database,
+        schema: [
+            {
+                measurement: 'servers',
+                fields: {
+                    online: Influx.FieldType.BOOLEAN,
+                    numplayers: Influx.FieldType.INTEGER
+                },
+                tags: [ 'server' ]
+            }
+        ]
+    }));
+
+    const databases = await influx.getDatabaseNames();
+
+    if(!databases.includes(database)){
         logger.info(`InfluxDB: database is not exists. create one: ${database}`);
         influx.createDatabase(database);
     }
-})();
-
-influx.waitForConnection = influx.ping(5000);
+};
 
 module.exports = influx;
